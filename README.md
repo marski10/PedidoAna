c919435df282e47f00748065b2739397d414d178
marcos.aurelio <marquinhus.bill@hotmail.com>
2024-04-09 23:27:27 -0300

    feat: :sparkles: Primeiros passos pedido casamento
    Criado novo layout da primeira tela de sim/não, ajustado tela após botão aceitar, e redirecionamento.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# PedidoAna
-Pedido de Namoro
+      <div class="heart">❤</div>
+        <div class="text">  <p>Q</p>
+            <p>U</p>
+            <p>E</p>
+            <p>R</p>
+            <p> </p>
+            <p>C</p>
+            <p>A</p>
+            <p>S</p>
+            <p>A</p>
+            <p>R</p>
+            <p> </p>
+            <p>C</p>
+            <p>O</p>
+            <p>M</p>
+            <p>I</p>
+            <p>G</p>
+            <p>O</p>
+            <p>?</p></div>
--- script.js
+++ script.js
@@ -1,41 +1,41 @@
 function redirectToOtherPage() {
-    // Altere a URL abaixo para a página para a qual deseja redirecionar.
+    // Altere a URL abaixo para a pï¿½gina para a qual deseja redirecionar.
     window.location.href = "./certeza/certeza.html";
 }
 
 function redirectToYesPage() {
-    // Altere a URL abaixo para a página para a qual deseja redirecionar.
+    // Altere a URL abaixo para a pï¿½gina para a qual deseja redirecionar.
     window.location.href = "./aceitou/aceitou.html";
 }
 
 
-function emitHearts() {
-    const heartContainer = document.querySelector('.heart-container');
+function emitheart2s() {
+    const heart2Container = document.querySelector('.heart2-container');
 
     for (let i = 0; i < 100; i++) {
-        const heart = document.createElement('div');
-        heart.classList.add('heart');
-        heart.style.left = Math.random() * 100 + 'vw';
-        heart.style.setProperty('--randomX', Math.random());
-        heart.style.animationDelay = Math.random() * 0.5 + 's';
+        const heart2 = document.createElement('div');
+        heart2.classList.add('heart2');
+        heart2.style.left = Math.random() * 100 + 'vw';
+        heart2.style.setProperty('--randomX', Math.random());
+        heart2.style.animationDelay = Math.random() * 0.5 + 's';
         
         const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
         svg.setAttribute("viewBox", "0 0 24 24");
         svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
         svg.innerHTML = `<path d="M12 21.35l-1.45-1.32C5.4 16.36 2 13.18 2 9.5 2 7.42 3.25 5.53 5.34 4.6C6.2 4.18 7.1 4 8 4c1.54 0 3.08 .59 4.24 1.76L12 7.25l1.76-1.49C14.92 4.59 16.46 4 18 4c.9 0 1.8 .18 2.66 .6C20.75 5.53 22 7.42 22 9.5c0 3.68-3.4 6.86-8.55 10.54L12 21.35z"/>`;
 
-        heart.appendChild(svg);
-        heartContainer.appendChild(heart);
+        heart2.appendChild(svg);
+        heart2Container.appendChild(heart2);
 
-         // Último coração criado
+         // ï¿½ltimo coraï¿½ï¿½o criado
          if (i === 99) {
-            heart.addEventListener('animationend', () => {
-                // Redirecionar para a outra página após a animação
+            heart2.addEventListener('animationend', () => {
+                // Redirecionar para a outra pï¿½gina apï¿½s a animaï¿½ï¿½o
                 redirectToYesPage();
             });
         } else {
-            heart.addEventListener('animationend', () => {
-                heart.remove();
+            heart2.addEventListener('animationend', () => {
+                heart2.remove();
             });
     }
 }
